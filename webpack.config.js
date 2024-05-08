820bad12bae954c4e60d91380a51ffc22e1c9bf5
Ivey Topaz <itopaz@paperlesspost.com>
2024-05-07 20:46:19 -0600

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    plugins: [HtmlWebpackPlugin({ title: 'D3 with TypeScript' })],
   },
+  plugins: [new HtmlWebpackPlugin({ title: 'D3 with TypeScript' })],
 };
